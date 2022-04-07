3fb499b0421ecfe7374402eb1ea25bb242cbfdb4
ReneAljou <85430250+ReneAljou@users.noreply.github.com>
2022-04-07 12:34:18 +0200

    Create to_dict.py
    Update to_dict.py

---
+++ to_dict.py
@@ -0,0 +1,18 @@
+list = ["dog", "cat", "wolf", "elephant", "lion"]
+
+element_1 = list[0]
+element_2 = list[1]
+element_3 = list[2]
+element_4 = list[3]
+element_5 = list[4]
+
+dict = {
+
+    element_1: element_1,
+    element_2: element_2,
+    element_3: element_3,
+    element_4: element_4,
+    element_5: element_5
+}
+
+print(dict)
